f8dee564e64740f5ac5ab757af502a49aaac67da
Sarah Preston <seprest@gmail.com>
2022-08-02 19:44:27 -0500

    first draft of javascript

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -10,35 +10,59 @@ function hello() {
 }
 // Call the function to test
 console.log('Test - should say "Hello World!"', hello());
+  //Was I supposed to do something with this question, or was it just an example?
+  //It looks completed, to me.
+
+
+
 
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( name ) {
-  return;
+function helloName(name) {
+  return `Hello, ${name}!`;
 }
 // Remember to call the function to test
 
+console.log('Test - should say "Hello, Sarah!"', helloName('Sarah'));
+    //I put my name in, to test. 
+
+
+
+
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
-  // return firstNumber + secondNumber;
+function addNumbers( firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 }
+
+console.log(`Sum of two numbers = ${addNumbers(3, 4)}`);
+
+
+
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree(num1, num2, num3){
+  return num1 * num2 * num3
 }
+
+console.log(`Multiplied total = ${multiplyThree(1, 2, 6)}`);
+  //curious about operation of orders with this :)
+
+
+
 
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return true;
   }
-    return;
+  else{
+    return false;
+  }
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
@@ -47,18 +71,46 @@ console.log( 'isPositive - should say false', isPositive(0) );
 console.log( 'isPositive - should say false', isPositive(-3) );
 
 
+
+
+
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-function getLast( array ) {
+function getLast( fruitArray ) {
+  return fruitArray[fruitArray.length-1];
+  }
 
-}
+let fruitArray =['apple', 'orange', 'kiwi'];
+console.log(`Test - ${getLast(fruitArray)}`);
+
+
+
+
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
-function find( value, array ){
-  
+function find( value, colorArray ){
+  for (i=0; i<colorArray.length; i++){
+    if (value===colorArray[i]){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
 }
+
+let colorArray = ['red', 'green', 'blue', 'purple', 12]
+console.log(`Test - ${find('green', fruitArray)}`)
+console.log(`Test - ${find('purple', fruitArray)}`)
+console.log(`Test - ${find(20, fruitArray)}`)
+
+//I DON'T KNOW WHAT'S WRONG!!!!!!!!!!!!!!!!
+
+
+
+
 
 // ----------------------
 // Stretch Goals
@@ -71,18 +123,48 @@ function isFirstLetter(letter, string) {
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
+
+
+
+
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll(numberArray) {
   let sum = 0
-  // TODO: loop to add items
+  for (i=0; i<numberArray.length; i++){
+     sum += numberArray[i];
+  }
   return sum;
 }
+  
+let numberArray = [20, 10, 100]
+console.log(`Test - ${sumAll(numberArray)}`)
+
+ 
+
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
+function posNumber(firstArray){
+  return firstArray[i] > 0;
+}
 
+function newArray(firstArray){
+  for (value of firstArray){
+    if (value > 0){
+      secondArray = [firstArray.filter(posNumber)];
+      return secondArray;
+    }
+    else{
+      return emptyArray=[];
+    }
+  }
+}
+
+
+let firstArray = [1, 3, 4, -8, -7, -9]
+console.log(newArray(firstArray));
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
